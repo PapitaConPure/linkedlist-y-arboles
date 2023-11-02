1decd33d47206035abd23e2b8ddfbaa56418c6f9
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2023-11-02 00:01:32 -0300

    bugfix

--- Estructuras/No Genéricas/Lista Ligada/ListaLigada.cs
+++ Estructuras/No Genéricas/Lista Ligada/ListaLigada.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Estructuras {
 	/// <summary>
@@ -76,7 +71,7 @@ namespace Estructuras {
 		/// <exception cref="ArgumentOutOfRangeException"></exception>
 		public object this[int idx] {
 			get {
-				return this.BuscarNodoPorÍndice(idx);
+				return this.BuscarNodoPorÍndice(idx).Valor;
 			}
 			set {
 				this.BuscarNodoPorÍndice(idx).Valor = value;
