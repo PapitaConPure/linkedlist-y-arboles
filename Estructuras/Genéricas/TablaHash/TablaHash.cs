49bc6b1fc33453f93a3c3032772998caf70f6a5e
PapitaConPure <33335038+PapitaConPure@users.noreply.github.com>
2023-11-06 01:49:23 -0300

    Documentar y mejorar TablaHash

--- Estructuras/Genéricas/TablaHash/TablaHash.cs
+++ Estructuras/Genéricas/TablaHash/TablaHash.cs
@@ -7,13 +7,21 @@ namespace Estructuras.Genéricas {
 		private NodoTablaHash<TClave, TValor>[] tabla;
 		private readonly double coberturaMáxima;
 		private readonly double factorCrecimiento;
+		private readonly int capacidadInicial;
 
 		public int Cantidad { get; private set; }
 
+		/// <summary>
+		/// Indica el límite de índices que puede contener la <see cref="TablaHash{TClave, TValor}"/>
+		/// </summary>
+		/// <remarks>Esto no representa la cantidad de elementos de la colección. Usa <see cref="Cantidad"/> para obtener eso</remarks>
 		public int Capacidad {
 			get { return this.tabla.Length; }
 		}
 
+		/// <summary>
+		/// Indica el porcentaje de cobertura de los índices de la <see cref="TablaHash{TClave, TValor}"/>
+		/// </summary>
 		public double Cobertura {
 			get {
 				int cnt = 0;
@@ -74,6 +82,14 @@ namespace Estructuras.Genéricas {
 			}
 		}
 
+		/// <summary>
+		/// Crea una nueva instancia de <see cref="TablaHash{TClave, TValor}"/> vacía,
+		/// con la <paramref name="capacidad"/> especificada y el manejo indicado de la misma
+		/// </summary>
+		/// <param name="capacidad">Cantidad de índices que alberga la <see cref="TablaHash{TClave, TValor}"/> inicialmente</param>
+		/// <param name="coberturaMáxima">Porcentaje de cobertura de los índices en el cual se realizará una redimensión de capacidad</param>
+		/// <param name="factorCrecimiento">El factor de crecimiento de la capacidad al realizar una redimensión</param>
+		/// <exception cref="ArgumentOutOfRangeException"></exception>
 		public TablaHash(int capacidad, double coberturaMáxima, double factorCrecimiento) {
 			if(capacidad < 2)
 				throw new ArgumentOutOfRangeException("La capacidad debe ser al menos 2");
@@ -85,15 +101,35 @@ namespace Estructuras.Genéricas {
 				throw new ArgumentOutOfRangeException("El factor de crecimiento debe ser un valor entre 1.2 y 4.0 inclusive");
 
 			this.tabla = new NodoTablaHash<TClave, TValor>[capacidad];
+			this.capacidadInicial = this.Capacidad;
 			this.Cantidad = 0;
 			this.coberturaMáxima = coberturaMáxima;
 			this.factorCrecimiento = factorCrecimiento;
 		}
 
+		/// <summary>
+		/// Crea una nueva instancia de <see cref="TablaHash{TClave, TValor}"/> vacía,
+		/// con la <paramref name="capacidad"/> y porcentaje de <paramref name="coberturaMáxima"/> especificados.
+		/// </summary>
+		/// <remarks>Las redimensiones de la tabla se harán con un factor de crecimiento de 1.5</remarks>
+		/// <param name="capacidad">Cantidad de índices que alberga la <see cref="TablaHash{TClave, TValor}"/> inicialmente</param>
+		/// <param name="coberturaMáxima">Porcentaje de cobertura de los índices en el cual se realizará una redimensión de capacidad</param>
+		/// <exception cref="ArgumentOutOfRangeException"></exception>
 		public TablaHash(int capacidad, double coberturaMáxima): this(capacidad, coberturaMáxima, 1.5) {}
-		
+
+		/// <summary>
+		/// Crea una nueva instancia de <see cref="TablaHash{TClave, TValor}"/> vacía, con la <paramref name="capacidad"/> especificada
+		/// </summary>
+		/// <remarks>Las redimensiones de la tabla se harán al superar una cobertura de 0.8, con crecimiento de x1.5</remarks>
+		/// <param name="capacidad">Cantidad de índices que alberga la <see cref="TablaHash{TClave, TValor}"/> inicialmente</param>
+		/// <exception cref="ArgumentOutOfRangeException"></exception>
 		public TablaHash(int capacidad): this(capacidad, 0.8) {}
 
+		/// <summary>
+		/// Crea una nueva instancia de <see cref="TablaHash{TClave, TValor}"/> vacía, con una capacidad inicial de 12 índices
+		/// </summary>
+		/// <remarks>Las redimensiones de la tabla se harán al superar una cobertura de 0.8, con crecimiento de x1.5</remarks>
+		/// <exception cref="ArgumentOutOfRangeException"></exception>
 		public TablaHash(): this(12) {}
 
 		public bool Insertar(TClave clave, TValor valor) {
@@ -109,7 +145,7 @@ namespace Estructuras.Genéricas {
 			if(nodo is null) {
 				this.tabla[idx] = new NodoTablaHash<TClave, TValor>(clave, valor);
 				this.Cantidad++;
-				this.ReescalarArriba();
+				this.RedimensionarArriba();
 				return true;
 			}
 
@@ -215,7 +251,7 @@ namespace Estructuras.Genéricas {
 
 			bool encontrado = false;
 
-			while(!encontrado && nodo.Siguiente is object) {
+			while(!encontrado && nodo is object) {
 				if(nodo.Clave.Equals(parOrdenado.Clave) && nodo.Valor.Equals(parOrdenado.Valor))
 					encontrado = true;
 				else
@@ -234,7 +270,7 @@ namespace Estructuras.Genéricas {
 
 			bool encontrado = false;
 
-			while(!encontrado && nodo.Siguiente is object) {
+			while(!encontrado && nodo is object) {
 				if(nodo.Clave.Equals(clave))
 					encontrado = true;
 				else
@@ -244,6 +280,11 @@ namespace Estructuras.Genéricas {
 			return encontrado;
 		}
 
+		/// <summary>
+		/// Revisa la <see cref="TablaHash{TClave, TValor}"/> en busca del <paramref name="valor"/> especificado  e indica si se encontró o no
+		/// </summary>
+		/// <param name="valor">Valor a buscar</param>
+		/// <returns><see langword="true"/> si se encontró, o <see langword="false"/> de lo contrario</returns>
 		public bool ContieneValor(TValor valor) {
 			bool encontrado = false;
 
@@ -269,52 +310,42 @@ namespace Estructuras.Genéricas {
 			this.Cantidad = 0;
 		}
 
-		public ParOrdenado<TClave, TValor>[] AVector(int índiceInicio, int cantidad = -1) {
+		/// <summary>
+		/// Quita todos los elementos de la <see cref="TablaHash{TClave, TValor}"/> y restaura su capacidad inicial
+		/// </summary>
+		public void Reestablecer() {
+			this.tabla = new NodoTablaHash<TClave, TValor>[this.capacidadInicial];
+			this.Cantidad = 0;
+		}
+
+		/// <inheritdoc cref="IColección{T}.AVector()"/>
+		/// <remarks>Los pares clave-valor no tienen un orden de utilidad</remarks>
+		public ParOrdenado<TClave, TValor>[] AVector() {
 			ParOrdenado<TClave, TValor>[] pares = new ParOrdenado<TClave, TValor>[this.Cantidad];
-
-			NodoTablaHash<TClave, TValor> nodo;
-			int c = índiceInicio;
-
-			if(cantidad < 0)
-				cantidad = this.Cantidad;
-
-			for(int i = 0; c < cantidad && i < this.Capacidad; i++) {
-				nodo = this.tabla[i];
-
-				while(c < cantidad && nodo is object) {
-					pares[c++] = nodo.AParOrdenado();
-					nodo = nodo.Siguiente;
-				}
-			}
-
+			this.CopiarEn(pares);
 			return pares;
 		}
 
-		public ParOrdenado<TClave, TValor>[] AVector() {
-			return this.AVector(0);
-		}
-
-		public void CopiarEn(ParOrdenado<TClave, TValor>[] destino, int índiceInicio, int cantidad = -1) {
+		/// <inheritdoc cref="IColección{T}.CopiarEn(T[])"/>
+		/// <remarks>Los pares clave-valor no tienen un orden de utilidad</remarks>
+		public void CopiarEn(ParOrdenado<TClave, TValor>[] destino) {
 			NodoTablaHash<TClave, TValor> nodo;
-			int c = índiceInicio;
 
-			if(cantidad < 0)
-				cantidad = this.Cantidad;
-
-			for(int i = 0; c < cantidad && i < this.Capacidad; i++) {
+			int c = 0;
+			for(int i = 0; i < this.Capacidad; i++) {
 				nodo = this.tabla[i];
 
-				while(c < cantidad && nodo is object) {
+				while(nodo is object) {
 					destino[c++] = nodo.AParOrdenado();
 					nodo = nodo.Siguiente;
 				}
 			}
 		}
 
-		public void CopiarEn(ParOrdenado<TClave, TValor>[] destino) {
-			this.CopiarEn(destino, 0);
-		}
-
+		/// <summary>
+		/// Recalcula la capacidad de la <see cref="TablaHash{TClave, TValor}"/> a un mínimo estable predecido
+		/// </summary>
+		/// <remarks>Requiere realocar todas las entradas y el tamaño resultante puede ser mayor al actual</remarks>
 		public void MinimizarCapacidad() {
 			int nuevaCapacidad = (int)Math.Ceiling(this.Capacidad * this.Cobertura / this.coberturaMáxima);
 
@@ -334,7 +365,12 @@ namespace Estructuras.Genéricas {
 			}
 		}
 
-		private void ReescalarArriba() {
+		/// <summary>
+		/// Verifica si hay que redimensionar la <see cref="TablaHash{TClave, TValor}"/>.
+		/// Si es el caso, la capacidad incrementa en función del factor de crecimiento
+		/// </summary>
+		/// <remarks>Redimensionar la tabla involucra reingresar todas las entradas, por lo que debe regularse cuidadosamente</remarks>
+		private void RedimensionarArriba() {
 			if(this.Cobertura <= this.coberturaMáxima)
 				return;
 
@@ -354,6 +390,13 @@ namespace Estructuras.Genéricas {
 			}
 		}
 
+		/// <summary>
+		/// Llama <see cref="object.GetHashCode()"/> para la <paramref name="clave"/> especificada
+		/// y restringe el resultado a la capacidad de la <see cref="TablaHash{TClave, TValor}"/>, actuando como un índice
+		/// </summary>
+		/// <remarks>El índice de tabla obtenido siempre va a ser válido</remarks>
+		/// <param name="clave">Clave usada para el cálculo de Hash</param>
+		/// <returns>El índice de tabla calculado para la clave ingresada</returns>
 		private int Hashear(TClave clave) {
 			int código = clave.GetHashCode();
 			int idx = Math.Abs(código % this.Capacidad);
